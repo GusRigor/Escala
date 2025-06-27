9a48e5ac7d0234a46d1336ebe609776dcdca6a96
GusRigor <gusrigor@icloud.com>
2025-06-26 22:11:31 -0500

    ✨ Add MemoriaCalculo

--- classes/escala.py
+++ classes/escala.py
@@ -1,13 +1,21 @@
+from classes.memoriaCalculo import MemoriaCalculo
+from classes.calendario import Calendario
+
 class Escala:
-    def __init__(self, instituicao, residente, setores, preceptores, calendario):
+    def __init__(self, instituicao, residente, setores, preceptores, calendario:Calendario):
         self.instituicao = instituicao
         self.residente = residente
         self.setores = setores
         self.preceptores = preceptores
         self.calendario = calendario
+        self.memoria_calculo_hospital = MemoriaCalculo('P', calendario.atividades)
+        self.memoria_calculo_teorica = MemoriaCalculo('T', calendario.atividades)
+
 
     def __str__(self):
         return f"Escala de {self.residente} na {self.instituicao}:\n" \
                f"Setores: {', '.join(self.setores)}\n" \
                f"Preceptores: {', '.join(self.preceptores)}\n" \
-               f"Calendário: {self.calendario}"
+               f"Calendário: {self.calendario}\n" \
+               f"Memória de cálculo hospitalar: {self.memoria_calculo_hospital.descritivo} = {self.memoria_calculo_hospital.total}h\n" \
+               f"Memória de cálculo teórica: {self.memoria_calculo_teorica.descritivo} = {self.memoria_calculo_teorica.total}h"
---
+++ classes/memoriaCalculo.py
@@ -0,0 +1,48 @@
+from classes.atividade import Atividade
+
+class MemoriaCalculo:
+    def __init__(self, tipo, atividades:list[Atividade]):
+        self.tipo = tipo
+        self.total = 0
+        self.descritivo = []
+        self._calculo_total_(atividades)
+        self._descritivo_tipo_atividade_(atividades)
+
+    def _calculo_total_(self, atividades):
+        for atividade in atividades:
+            if atividade.tipo.tipo == self.tipo:
+                self.total += atividade.tipo.duracao
+
+    def _descritivo_tipo_atividade_(self, atividades:list[Atividade]):
+        siglas = self._siglas_unicas_(atividades)
+        quantidade_por_atividade = self._quantidade_por_atividade_(atividades, siglas)
+        self.descritivo = self._quantidade_por_atividade_para_descritivo_(quantidade_por_atividade)
+    
+    def _siglas_unicas_(self, atividades:list[Atividade]):
+        siglas = set()
+        for atividade in atividades:
+            if atividade.tipo.tipo == self.tipo:
+                siglas.add(atividade.tipo.sigla)
+        return siglas
+    
+    def _quantidade_por_atividade_(self, atividades:list[Atividade], siglas):
+        quantidade_por_atividade = []
+        for sigla in siglas:
+            quantidade = sum(1 for atividade in atividades if atividade.tipo.sigla == sigla)
+            atividade = self._atividade_para_sigla_(atividades, sigla)
+            quantidade_por_atividade.append({'atividade': atividade, 'quantidade': quantidade})
+
+        return quantidade_por_atividade
+    
+    def _atividade_para_sigla_(self, atividades:list[Atividade], sigla):
+        for atividade in atividades:
+            if atividade.tipo.sigla == sigla:
+                return atividade
+            
+    def _quantidade_por_atividade_para_descritivo_(self, quantidade_por_atividade):
+        descritivo = []
+        for item in quantidade_por_atividade:
+            atividade = item['atividade']
+            quantidade = item['quantidade']
+            descritivo.append(f"{atividade.tipo.sigla}-{atividade.tipo.duracao}={quantidade}x{atividade.tipo.duracao}={quantidade * atividade.tipo.duracao}h")
+        return descritivo
