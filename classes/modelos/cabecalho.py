637decf4bce025999b103c88290672f43d0be1f3
GusRigor <gusrigor@icloud.com>
2025-06-28 22:27:01 -0500

    ✨ Adicionar lógica de renderização para o cabeçalho e atualizar o template LaTeX

---
+++ classes/modelos/cabecalho.py
@@ -0,0 +1,32 @@
+from classes.modelos.Renderable import Renderable
+
+class Cabecalho(Renderable):
+
+    def __init__(self):
+        self.mes = "Junho"
+        self.ano = "2025"
+        self.instituicao = "Maternidade Bárbara Heliodora"
+        self.residente = "Milene Mendes da Silva"
+        self.setores = ["PPP"]
+        self.preceptores = ["Enfª Obst. Ana Cláudia"]
+
+    def __str__(self):
+        return f"Escala de {self.residente} na {self.instituicao} - {self.mes}/{self.ano}\n" \
+               f"Setores: {', '.join(self.setores)}\n" \
+               f"Preceptores: {', '.join(self.preceptores)}\n"
+    
+    def _setores_para_render_(self):
+        return " ".join(self.setores) if self.setores else ""
+    
+    def _preceptores_para_render_(self):
+        return " ".join(self.preceptores) if self.preceptores else ""
+
+    def render(self):
+        return {
+            "mes": self.mes,
+            "ano": self.ano,
+            "instituicao": self.instituicao,
+            "residente": self.residente,
+            "setores": self._setores_para_render_(),
+            "preceptores": self._preceptores_para_render_()
+        }
