95c1f19dc043d60787eb586668fd6452c08a4ecb
GusRigor <gusrigor@icloud.com>
2025-07-04 17:38:00 -0500

    ✨ Atualizar lógica de download de PDF para incluir timestamp no nome do arquivo e ajustar a ordem das atividades nos templates LaTeX

--- escala_web/escala/views/download_pdf.py
+++ escala_web/escala/views/download_pdf.py
@@ -1,9 +1,11 @@
 from django.http import FileResponse, Http404
 import os
+from datetime import datetime
 
 def download_pdf(request):
     caminho_pdf = os.path.join('source', 'latex', 'main.pdf')
     if not os.path.exists(caminho_pdf):
         raise Http404("Arquivo não encontrado")
 
-    return FileResponse(open(caminho_pdf, 'rb'), as_attachment=True, filename='escala.pdf')
+    filename = f'escala-{datetime.now()}.pdf'
+    return FileResponse(open(caminho_pdf, 'rb'), as_attachment=True, filename=filename)
